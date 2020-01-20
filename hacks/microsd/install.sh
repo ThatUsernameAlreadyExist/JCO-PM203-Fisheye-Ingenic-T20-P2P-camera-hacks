88d92d2a01ea760f90ee75eb0e78b642e3dede68
Alexander <ThatUsernameAlreadyExist@cock.li>
2020-01-20 23:13:22 +0200

    Update installation: add new firmware support('Release 2019-06-14') (#14)

--- hacks/microsd/install.sh
+++ hacks/microsd/install.sh
@@ -1,51 +1,34 @@
 #!/bin/sh
 
-DST_FILE_TO_PATCH="/opt/etc/lzbox"
-SRC_PATCHED_FILE="/opt/media/mmcblk0p1/patch/patched/lzbox"
-ORIGINAL_FILE="/opt/media/mmcblk0p1/patch/original/lzbox"
-DST_ORIGINAL_BACKUP_FILE="/opt/media/mmcblk0p1/patch/backup/lzbox"
-PATCH_ENABLE_FILE="/opt/media/mmcblk0p1/force_dbg.txt"
+DST_PATCH="/opt/etc/local.rc"
+SRC_PATCH="/opt/media/mmcblk0p1/patch/local.rc"
 AUTORUN_FILE="/ipc/etc/auto_run.sh"
 WIFI_SYSTEM_CONFIG="/opt/conf/airlink/supplicant.conf"
 WIFI_USER_CONFIG="/opt/media/mmcblk0p1/wpa_supplicant.conf"
-AUTORUN_FILE_TAG1="/opt/media/mmcblk0p1/force_dbg.txt"
-AUTORUN_FILE_TAG2="lzbox 0"
 
 . /opt/media/mmcblk0p1/scripts/common_functions.sh
 
 apply_patch()
 {
-    if ! is_files_equal "$DST_FILE_TO_PATCH" "$SRC_PATCHED_FILE"; then
-        cp -f $DST_FILE_TO_PATCH $DST_ORIGINAL_BACKUP_FILE
-    else
-        :
-    fi
-    
+    cp -f $SRC_PATCH $DST_PATCH
     if [ $? -ne 0 ] ; then
-        echo "ERROR: can't create backup of file $DST_FILE_TO_PATCH"
-        echo "ERROR: patch not applied"
+        echo "ERROR: can't write patch file to $DST_PATCH"
+        echo "ERROR: patch not installed!"
     else
-        cp -f $SRC_PATCHED_FILE $DST_FILE_TO_PATCH
-        if [ $? -ne 0 ] ; then
-            echo "ERROR: can't write patched file to $DST_FILE_TO_PATCH"
-            echo "ERROR: patch not installed!"
-        else
-            touch $PATCH_ENABLE_FILE
-            if [ ! -f $WIFI_USER_CONFIG ]; then
-                cp -f $WIFI_SYSTEM_CONFIG $WIFI_USER_CONFIG
-            fi
-            echo "SUCCESS install patch! Reboot in 3 seconds..."
-            sleep 3
-            reboot -f
+        chmod +x $DST_PATCH
+        if [ ! -f $WIFI_USER_CONFIG ]; then
+            cp -f $WIFI_SYSTEM_CONFIG $WIFI_USER_CONFIG
         fi
+        echo "SUCCESS install patch! Reboot in 3 seconds..."
+        sleep 3
+        reboot -f
     fi
 }
 
 ############################################################
 echo "=====Installing patch====="
 
-if (is_files_equal "$ORIGINAL_FILE" "$DST_FILE_TO_PATCH" || is_files_equal "$DST_FILE_TO_PATCH" "$SRC_PATCHED_FILE") && \
- is_file_contain_str "$AUTORUN_FILE" "$AUTORUN_FILE_TAG1" && is_file_contain_str "$AUTORUN_FILE" "$AUTORUN_FILE_TAG2"; then
+if is_file_contain_str "$AUTORUN_FILE" "$DST_PATCH"; then
     echo "System check success."
     apply_patch
 else
--- hacks/microsd/uninstall.sh
+++ hacks/microsd/uninstall.sh
@@ -1,34 +1,15 @@
 #!/bin/sh
 
-DST_FILE_TO_RESTORE="/opt/etc/lzbox"
-SRC_BACKUP_FILE="/opt/media/mmcblk0p1/patch/backup/lzbox"
-SRC_PATCHED_FILE="/opt/media/mmcblk0p1/patch/patched/lzbox"
-PATCH_ENABLE_FILE="/opt/media/mmcblk0p1/force_dbg.txt"
-
-. /opt/media/mmcblk0p1/scripts/common_functions.sh
+DST_PATCH="/opt/etc/local.rc"
 
 ############################################################
 echo "=====Uninstalling patch====="
 
-rm -f $PATCH_ENABLE_FILE
+rm -f $DST_PATCH
 if [ $? -ne 0 ] ; then
-    echo "WARNING: can't remove patch-enable file: $PATCH_ENABLE_FILE"
-fi
-
-if [ -f "$SRC_BACKUP_FILE" ]; then
-    if is_files_equal "$DST_FILE_TO_RESTORE" "$SRC_PATCHED_FILE"; then
-        cp -f $SRC_BACKUP_FILE $DST_FILE_TO_RESTORE
-        if [ $? -ne 0 ] ; then
-            echo "ERROR: can't restore file: $DST_FILE_TO_RESTORE"
-        else
-            rm -f "$SRC_BACKUP_FILE"
-            echo "Success uninstall patch! Reboot in 3 seconds..."
-            sleep 3
-            reboot -f
-        fi
-    else
-        echo "ERROR: found modified(original) file: $DST_FILE_TO_RESTORE"
-    fi
+    echo "ERROR: can't remove patch-enable file: $DST_PATCH"
 else
-    echo "ERROR: backup file not found: $SRC_BACKUP_FILE"
+    echo "Success uninstall patch! Reboot in 3 seconds..."
+    sleep 3
+    reboot -f
 fi
